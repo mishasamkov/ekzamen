e4f801bedf9525ca245a862c6087e9451d4238d4
kenn1d <makarkibanov220@gmail.com>
2025-12-30 08:45:55 +0500

    .

---
+++ News.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class News
+    {
+
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Src { get; set; }
+        public News(string name, string tag,DateTime dateTime,string src) {
+
+            this.Name = name;
+            this.Tag = tag;
+            this.DateTime = dateTime;
+            this.Src = src;
+        }
+    }
+}
